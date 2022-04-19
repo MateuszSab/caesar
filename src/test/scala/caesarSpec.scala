1ee595881afec330c3c894867ea0e26941d0a3ec
m.sabatowski <ma.sabatowski@gmail.com>
2022-04-19 10:41:51 +0200

    tests added

--- src/test/scala/caesarSpec.scala
+++ src/test/scala/caesarSpec.scala
@@ -1,10 +1,18 @@
-import caesar.caesar
+import caesar.{caesar, decipher}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 class caesarSpec extends AnyFlatSpec with should.Matchers{
 
   it should "return dailyprogrammer for (\"jgorevxumxgsskx\", 20)" in {
     caesar("jgorevxumxgsskx", 20) shouldBe "dailyprogrammer"
+  }
+
+  it should "return \"bcda\" for (\"abcz\", 1))" in {
+    caesar("abcz", 1) shouldBe "bcda"
+  }
+
+  it should "return m for (q, 22)" in {
+    decipher('q', 22) shouldBe 'm'
   }
 
 }
